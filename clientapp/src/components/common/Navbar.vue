483e9b5a2ec37dea4ab4190de9c5f88aac014ede
Fengche Yen <knight618134@gmail.com>
2020-09-29 18:22:43 +0800

    install bootstrap vue

--- clientapp/src/components/common/Navbar.vue
+++ clientapp/src/components/common/Navbar.vue
@@ -1,54 +1,54 @@
 <template>
-  <div class="header">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div
-        class="collapse navbar-collapse d-flex justify-content-between"
-        id="navbarTogglerDemo01"
-      >
-        <div>
-          <a class="navbar-brand ml-5" href="#">Xforum</a>
-        </div>
-        <div>
-          <ul class="navbar-nav mt-2">
-            <li class="nav-item mt-1">
-              <svg
-                width="1.5em"
-                height="1.5em"
-                viewBox="0 0 16 16"
-                class="bi bi-pencil-square"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"
-                />
-                <path
-                  fill-rule="evenodd"
-                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-                />
-              </svg>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link ml-3" href="#">Login</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link ml-3" href="#">Register</a>
-            </li>
-            <input
-              class="form-control ml-3"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success ml-3" type="submit">
-              Search
-            </button>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
 
-    <vue-particles
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit"
+              >Search</b-button
+            >
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+
+  <!-- header role="banner" -->
+  <!-- end container -->
+  <!--# Navbar #-->
+
+  <!-- <vue-particles
       color="#dedede"
       :particleOpacity="0.7"
       :particlesNumber="80"
@@ -65,12 +65,9 @@
       :clickEffect="false"
       clickMode="push"
     >
-    </vue-particles>
-  </div>
+    </vue-particles> -->
 </template>
 
-<script>
-export default {};
-</script>
+<script></script>
 
 <style lang="scss" scoped></style>
--- clientapp/src/main.js
+++ clientapp/src/main.js
@@ -2,8 +2,10 @@ import Vue from "vue";
 import Router from "./router";
 import App from "./App.vue";
 import VueParticles from "vue-particles";
+import BoostrapVue from "bootstrap-vue";
 
 Vue.use(VueParticles);
+Vue.use(BoostrapVue);
 
 new Vue({
   el: "#app",
