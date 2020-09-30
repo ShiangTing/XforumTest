9fb027a4a49d521b711aac638619560f66023389
Sharon Chen <treetwood@gmail.com>
2020-09-30 16:41:38 +0800

    created grepository

--- Context/XForomTestDBContext.cs
+++ Context/XForomTestDBContext.cs
@@ -39,14 +39,14 @@ namespace XforumTest.Context
         public virtual DbSet<Thread> Thread { get; set; }
         public virtual DbSet<WishList> WishList { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=xforumtest.database.windows.net,1433;Database=XForomTestDB;User=test;Password=!a1234567;");
-            }
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Server=xforumtest.database.windows.net,1433;Database=XForomTestDB;User=test;Password=!a1234567;");
+//            }
+//        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ Repository/GeneralRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XforumTest.Context;
+using XforumTest.Models;
+using XforumTest.NewFolder;
+
+namespace XforumTest.Repository
+{
+    public class GeneralRepository<T> : IRepository<T> where T :class
+    {
+        private static XForomTestDBContext context;
+
+        protected XForomTestDBContext Context
+        {
+            get { return context; }
+        }
+
+
+
+
+        public void Create(T entity)
+        {
+            context.Entry(entity).State = EntityState.Added;
+        }
+
+        public void Delete(T entity)
+        {
+            context.Entry(entity).State = EntityState.Deleted;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return context.Set<T>();
+        }
+
+        public T GetFirst(System.Linq.Expressions.Expression<Func<T, bool>> entity)
+        {
+            return context.Set<T>().FirstOrDefault<T>(entity);
+        }
+
+        public void SaveContext()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
