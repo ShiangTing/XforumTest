df410abe9cd023e3bdf6e22efdb7c977bc71918a
Joey_Wang <joeywang1991@gmail.com>
2020-10-04 16:57:54 +0800

    重建基底 10/04

---
+++ DTO/ForumGetAllDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XforumTest.ViewModels
+{
+    public class ForumGetAllDTO
+    {
+    }
+}
