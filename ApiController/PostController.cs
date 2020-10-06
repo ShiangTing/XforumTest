e15a104af67a4f262bb93b7e6690544a6548c701
Joey_Wang <joeywang1991@gmail.com>
2020-10-06 19:38:48 +0800

    新增 Post、Forum api

---
+++ ApiController/PostController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using XforumTest.Context;
+using XforumTest.DTO;
+using XforumTest.Services;
+
+namespace XforumTest.ApiController
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private static PostService post = new PostService();
+        [HttpPost]
+        public IActionResult Create(PostDto po)
+        {
+            if (ModelState.IsValid)
+            {
+                post.Create(po);
+                return Ok();
+            }
+            return Ok();
+        }
+
+        //軟刪除
+        [HttpPost]
+        void Delete()
+        {
+        }
+
+        //編輯 留言資訊
+        [HttpGet]
+        void GetSingle()
+        {
+
+        }
+        [HttpPost]
+        void Edit()
+        {
+
+        }
+        //取得所有留言資訊
+        [HttpGet]
+        void GetAll()
+        {
+
+        }
+
+    }
+}
--- DataTable/Posts.cs
+++ DataTable/Posts.cs
@@ -5,17 +5,22 @@ namespace XforumTest.DataTable
 {
     public partial class Posts
     {
+        public Posts()
+        {
+            ReposiveMessages = new HashSet<ReposiveMessages>();
+        }
+
         public Guid PostId { get; set; }
+        public Guid? ForumId { get; set; }
+        public Guid? UserId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Img { get; set; }
-        public Guid? ForumId { get; set; }
-        public int? LikeNumber { get; set; }
-        public int? DisLikeNumber { get; set; }
         public DateTime? CreatedDate { get; set; }
-        public Guid? UserId { get; set; }
+        public string Img { get; set; }
+        public bool? State { get; set; }
 
         public virtual Forums Forum { get; set; }
         public virtual ForumMembers User { get; set; }
+        public virtual ICollection<ReposiveMessages> ReposiveMessages { get; set; }
     }
 }
