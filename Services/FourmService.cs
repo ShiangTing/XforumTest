ced95351c3f5b58127062f62a489a9853fd1183d
Sharon Chen <treetwood@gmail.com>
2020-10-06 16:03:43 +0800

    fixed error

--- Services/FourmService.cs
+++ Services/FourmService.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using XforumTest.Context;
 using XforumTest.DataTable;
+using XforumTest.DTO;
 using XforumTest.Interface;
 using XforumTest.Repository;
 
 namespace XforumTest.Services
 {
-    public class FourmService : IFourmService
+    public class FourmService : IForumService
     {
         static MyDBContext db = new MyDBContext();
         GeneralRepository<Forums> forums = new GeneralRepository<Forums>(db);
@@ -30,12 +31,22 @@ namespace XforumTest.Services
             throw new NotImplementedException();
         }
 
+        public IQueryable Edit(string forumid)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Find()
         {
             throw new NotImplementedException();
         }
 
         public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        IQueryable<ForumGetAllDTO> IForumService.GetAll()
         {
             throw new NotImplementedException();
         }
