bcd744a41450c42b4dc911b5cadd57753250275c
Sharon Chen <treetwood@gmail.com>
2020-10-07 18:41:27 +0800

    added apiresult

---
+++ Services/ApiResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XforumTest.Services
+{
+   
+
+    public class ApiResult
+    {
+
+        public ApiResult(int status,string err,Object result)
+        {
+            Status = status;
+            ErrorMsg = err;
+            Result = result;
+
+        }
+        //public class ApiResult(int status,string err, Object result)
+        //{
+        //    Stat
+        //}
+        public int Status { get; set; }
+        public string ErrorMsg { get; set; }
+        public Object Result { get; set; }
+
+        public static class APIStatus
+        {
+            public static int Success = 0;
+            public static int Error = 1;
+        }
+    }
+ 
+}
