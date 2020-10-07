cfa951f636b0a5eb4fd5409cb1c17db5b7749a97
Sharon Chen <treetwood@gmail.com>
2020-10-07 13:08:52 +0800

    added PostListDto and PostService GetAll function

---
+++ DTO/PostListDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XforumTest.DTO
+{
+    public class PostListDto
+    {
+        public Guid? ForumId { get; set; }
+        public Guid? PostId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        //public string Img { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public Guid? UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
