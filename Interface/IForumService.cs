48b7163c999d8238091a94a4d096dec702ed088b
Joey_Wang <joeywang1991@gmail.com>
2020-10-10 17:17:09 +0800

    合併衝突

--- Interface/IForumService.cs
+++ Interface/IForumService.cs
@@ -11,7 +11,7 @@ namespace XforumTest.Interface
     public interface IForumService
     {
         void Create(ForumCreate create);
-        IList<ForumGetAllDTO> GetAll();
+        IQueryable<ForumGetAllDTO> GetAll();
         IQueryable GetSingle(string forumid);   
         void Delete(string id);
         void Edit(ForumCreate json);
