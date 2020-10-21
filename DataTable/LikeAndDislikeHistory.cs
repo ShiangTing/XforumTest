0019e6582184e0a106c815523380fdf16972f439
Sharon Chen <treetwood@gmail.com>
2020-10-21 17:31:13 +0800

    added History/Chat/Category table

---
+++ DataTable/LikeAndDislikeHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XforumTest.DataTable
+{
+    public partial class LikeAndDislikeHistory
+    {
+        public Guid? UserId { get; set; }
+        public Guid? PostId { get; set; }
+        public Guid? MessageId { get; set; }
+        public Guid Id { get; set; }
+
+        public virtual ReposiveMessages Message { get; set; }
+        public virtual Posts Post { get; set; }
+        public virtual ForumMembers User { get; set; }
+    }
+}
