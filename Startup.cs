da8b63cbe1cc40aaee15780fed0eb3d9f6a6f002
Sharon Chen <treetwood@gmail.com>
2020-10-26 19:53:59 +0800

    set the config to deploye

--- Startup.cs
+++ Startup.cs
@@ -129,6 +129,23 @@ namespace XforumTest
             //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             //    app.UseHsts();
             //}
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404 &&                       // 該資源不存在
+                    !System.IO.Path.HasExtension(context.Request.Path.Value) && // 網址最後沒有帶副檔名
+                    !context.Request.Path.Value.StartsWith("/api"))             // 網址不是 /api 開頭（不是發送 API 需求）
+                {
+                    context.Request.Path = "/index.html";                       // 將網址改成 /index.html
+                    context.Response.StatusCode = 200;                          // 並將 HTTP 狀態碼修改為 200 成功
+
+                    await next();
+                }
+
+
+
+            });
             app.UseHttpsRedirection();
 
             app.UseCors("CorsPolicy");
@@ -148,7 +165,7 @@ namespace XforumTest
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
-
+            app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthentication();
