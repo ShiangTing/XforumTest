3d580689b867c7d72f7c7e684631b9e6cef9c915
Sharon Chen <treetwood@gmail.com>
2020-10-28 21:30:20 +0800

    cancled the note in startup

--- Startup.cs
+++ Startup.cs
@@ -115,20 +115,20 @@ namespace XforumTest
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //app.Use(async (context, next) =>
-            //{
-            //    await next();
+            app.Use(async (context, next) =>
+            {
+                await next();
 
-            //    if (context.Response.StatusCode == 404 &&
-            //        !System.IO.Path.HasExtension(context.Request.Path.Value) &&
-            //        !context.Request.Path.Value.StartsWith("/api"))
-            //    {
-            //        context.Request.Path = "/index.html";
-            //        context.Response.StatusCode = 200;
+                if (context.Response.StatusCode == 404 &&
+                    !System.IO.Path.HasExtension(context.Request.Path.Value) &&
+                    !context.Request.Path.Value.StartsWith("/api"))
+                {
+                    context.Request.Path = "/index.html";
+                    context.Response.StatusCode = 200;
 
-            //        await next();
-            //    }
-            //});
+                    await next();
+                }
+            });
             app.UseExceptionHandler("/api/error/Error");
             app.UseHttpsRedirection();
 
@@ -143,7 +143,7 @@ namespace XforumTest
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
-           //app.UseDefaultFiles();
+           app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthentication();
