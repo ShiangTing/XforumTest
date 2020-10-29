439484cfbcb788534dcff1701b62dbfbdfe34a32
PG <frankpg267@gmail.com>
2020-10-29 16:45:56 +0800

    新增ChatController api

--- ApiController/ChatController.cs
+++ ApiController/ChatController.cs
@@ -4,16 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using XforumTest.Hubs;
+using XforumTest.Interface;
 
 namespace XforumTest.ApiController
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ChatController : ControllerBase
     {
 
+        private readonly IHubContext<ChatHub, IChat> _hubContext;
+
+        public ChatController(IHubContext<ChatHub, IChat> hubContext)
+        {
+            _hubContext = hubContext;
+        }
 
 
 
+
+        [HttpGet]
+        public async Task GetMessage(string msg)
+        {
+            await _hubContext.Clients.All.ReceiveMessage(msg);
+        }
     }
 }
